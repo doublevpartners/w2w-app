cfc6d35559cb8bd5486789c955e2e6e79f2cb72f
JuankTique <juan.tique@doublevpartners.com>
2022-11-18 04:01:44 -0500

    Regular Expresion Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -42,70 +42,87 @@ class _LoginForm extends StatelessWidget {
     return Container(
       child: Form(
           //TODO: mantener la referencia al key
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-              autocorrect: false,
-              autofocus: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  labelText: 'Email',
-                  hintText: 'example@mail.com',
-                  prefixIcon: Icons.email_outlined)),
-          const SizedBox(
-            height: 15,
-          ),
-          TextFormField(
-            autocorrect: false,
-            autofocus: false,
-            obscureText: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                labelText: 'Password',
-                hintText: '************',
-                prefixIcon: Icons.lock_open),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            alignment: Alignment.topRight,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: const Color.fromARGB(129, 163, 159, 159)),
-              child: const Text(
-                'Olvidaste Contraseña?',
-                style: TextStyle(color: Colors.white),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                autofocus: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    labelText: 'Email',
+                    hintText: 'example@mail.com',
+                    prefixIcon: Icons.email_outlined),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+                  RegExp regExp = new RegExp(pattern);
+
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'Ups esto no es un correo';
+                },
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          MaterialButton(
-            disabledColor: Colors.grey,
-            elevation: 0,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            color: Colors.deepOrange,
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 150, vertical: 15),
-                child: const Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
-                )),
-            onPressed: () {
-              //TODO: Login Form
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          )
-        ],
-      )),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                autocorrect: false,
+                autofocus: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    labelText: 'Password',
+                    hintText: '************',
+                    prefixIcon: Icons.lock_open),
+                validator: (value) {
+                  if (value != null && value.length >= 8) return null;
+                  return 'La contraseña debe tener minimo 8 caracteres';
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color.fromARGB(129, 163, 159, 159)),
+                  child: const Text(
+                    'Olvidaste Contraseña?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MaterialButton(
+                disabledColor: Colors.grey,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                color: Colors.deepOrange,
+                child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 150, vertical: 15),
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: () {
+                  //TODO: Login Form
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          )),
     );
   }
 }
